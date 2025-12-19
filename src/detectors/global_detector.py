cd01121eb141530fd28687081e909f08125035d0
KhanhNH2420 <hoangkhanh2420@gmail.com>
2025-12-19 13:23:30 +0700

    upload ver5

--- src/detectors/global_detector.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def global_detector(rec_err, latent, contamination=0.01):
-    # Sử dụng reconstruction error + latent norm
-    latent_norm = np.linalg.norm(latent, axis=1)
-    score = rec_err + latent_norm
-    # The score is the anomaly score, higher is more anomalous
-    return score
--- src/detectors/sparse_detector.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.ensemble import IsolationForest
-
-def sparse_detector(Xp, contamination=0.01):
-    clf = IsolationForest(contamination=contamination, random_state=42)
-    clf.fit(Xp)
-    score = -clf.score_samples(Xp)  # higher -> anomaly
-    return score
